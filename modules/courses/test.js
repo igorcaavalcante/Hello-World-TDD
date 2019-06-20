d725359c06942e8a9a9b2e100b4456533a295824
Igor Cavalcante <igorcavlim@gmail.com>
2019-06-20 16:32:59 -0300

    Teacher.id

--- modules/courses/test.js
+++ modules/courses/test.js
@@ -26,12 +26,12 @@ describe('courses', () => {
     });
 
     it('shoud not create a course - title not provided', async () => {
-        const res = await request(server).post('/api/courses').send({ workLoad, places, teacher });
+        const res = await request(server).post('/api/courses').send({ workLoad, places, teacher: teacher.id });
         expect(res.status).toBe(400);
     });
 
     it('shoud not create a course - workLoad not provided', async () => {
-        const res = await request(server).post('/api/courses').send({ title, places, teacher });
+        const res = await request(server).post('/api/courses').send({ title, places, teacher: teacher.id });
         expect(res.status).toBe(400);
     });
 
@@ -41,7 +41,7 @@ describe('courses', () => {
     });
 
     it('shoud not create a course - number of places not provided', async () => {
-        const res = await request(server).post('/api/courses').send({ title, workLoad, teacher });
+        const res = await request(server).post('/api/courses').send({ title, workLoad, teacher: teacher.id });
         expect(res.status).toBe(400);
     });
 
@@ -52,7 +52,7 @@ describe('courses', () => {
     });
 
     it('shoud create a course - everything ok', async () => {
-        const res = await request(server).post('/api/courses').send({ title, workLoad, places, teacher });
+        const res = await request(server).post('/api/courses').send({ title, workLoad, places, teacher: teacher.id });
         expect(res.status).toBe(200);
     });
 
